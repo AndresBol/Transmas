e09ba50913c5c8bfe1257af0d257bbe9d5a5e4e0
Andrés Bolaños <andresbol1706@gmail.com>
2026-07-08 02:49:35 -0600

    Add Logger and update server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,35 +2,36 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
+import path from "path";
+import { AppRoutes } from "./routes/routes";
+import { ErrorMiddleware } from "./middlewares/error.middleware";
 
 const app = express();
-// Acceder a la configuracion del archivo .env
+
 dotenv.config();
-// Puerto que escucha por defecto 300 o definido .env
+
 const port = process.env.PORT || 3000;
-// Middleware CORS para aceptar llamadas en el servido
+
 app.use(cors());
-// Middleware para loggear las llamadas al servidor
 app.use(morgan("dev"));
-// Middleware para gestionar Requests y Response json
 app.use(express.json());
 app.use(
-  express.urlencoded({
-    extended: true,
-  }),
+    express.urlencoded({
+        extended: true,
+    })
 );
+
 app.get("/", (req, res) => {
-  res.json({
-    message: "Transmas API funcionando correctamente",
-  });
+    res.json({
+        message: "Transmas API funcionando correctamente",
+    });
 });
-//---- Definir rutas ----
 
-// Handle errors middleware
-
-//Acceso a las imágenes
+app.use(AppRoutes.routes);
+app.use("/images", express.static(path.join(path.resolve(), "assets/uploads")));
+app.use(ErrorMiddleware.handleError);
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-  console.log("Presione CTRL-C para detenerlo\n");
+    console.log(`http://localhost:${port}`);
+    console.log("Presione CTRL-C para detenerlo\n");
 });
